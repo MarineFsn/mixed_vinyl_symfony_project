4a5ef351ebfa95d433834b0a242975b7198aafd8
Marine <fassma89@gmail.com>
2023-09-04 15:48:13 +0200

    route ->controller->response

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\Query\AST\Functions\UpperFunction;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VinylController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response 
+    {
+        if($slug){
+        $title = 'Genre: '.u(str_replace('-',' ',$slug))->title(true);
+    } else {
+        $title ='all Genres';
+    }
+        return new Response('Genre: ' .$title);
+    }
+}
